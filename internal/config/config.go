f05050e6d7d8bc3107d0e4c88146cefcc6921630
Qais Patankar <qaisjp@gmail.com>
2019-01-31 23:25:09 +0000

    Add uuid to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,13 @@
 package config
 
+import "github.com/google/uuid"
+
 type Config struct {
 	LogLevel string `default:"debug"`
 
 	// Bind Address
 	BindAddress string `default:"0.0.0.0:8080"`
+
+	// Static Robot UUID (stage 1 only)
+	UUID uuid.UUID `required:"true"`
 }
