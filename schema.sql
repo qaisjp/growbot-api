81461367f181a261f4694ea8d07f939ebd1d3f8a
Qais Patankar <qaisjp@gmail.com>
2019-02-22 00:15:37 +0000

    db: add rooms table

--- schema.sql
+++ schema.sql
@@ -20,6 +20,43 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: rooms; Type: TABLE; Schema: public; Owner: growbot
+--
+
+CREATE TABLE public.rooms (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    name text NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    updated_at time without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.rooms OWNER TO growbot;
+
+--
+-- Name: rooms_id_seq; Type: SEQUENCE; Schema: public; Owner: growbot
+--
+
+CREATE SEQUENCE public.rooms_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.rooms_id_seq OWNER TO growbot;
+
+--
+-- Name: rooms_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: growbot
+--
+
+ALTER SEQUENCE public.rooms_id_seq OWNED BY public.rooms.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: growbot
 --
 
@@ -60,10 +97,40 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: rooms id; Type: DEFAULT; Schema: public; Owner: growbot
+--
+
+ALTER TABLE ONLY public.rooms ALTER COLUMN id SET DEFAULT nextval('public.rooms_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: growbot
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: rooms rooms_id_pkey; Type: CONSTRAINT; Schema: public; Owner: growbot
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_id_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rooms rooms_user_id_name_key; Type: CONSTRAINT; Schema: public; Owner: growbot
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_user_id_name_key UNIQUE (user_id, name);
+
+
+--
+-- Name: CONSTRAINT rooms_user_id_name_key ON rooms; Type: COMMENT; Schema: public; Owner: growbot
+--
+
+COMMENT ON CONSTRAINT rooms_user_id_name_key ON public.rooms IS 'A user cannot have multiple rooms with the same name';
 
 
 --
@@ -80,6 +147,14 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_id_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rooms rooms_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: growbot
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
 --
