7c1fa5ff0fdf82796ed2e3e27d876c26c12ed2d7
Qais Patankar <qaisjp@gmail.com>
2019-03-11 23:52:31 +0000

    fix /events

--- internal/models/schedule.go
+++ internal/models/schedule.go
@@ -3,28 +3,28 @@ package models
 import (
 	"time"
 
+	"github.com/lib/pq"
+
 	"github.com/jmoiron/sqlx/types"
 )
 
 type Event struct {
-	ID         int       `json:"id" db:"id"`
-	Summary    string    `json:"summary" db:"summary"`
-	Start      time.Time `json:"start" db:"start"`
-	Recurrence []string  `json:"recurrence" db:"recurrence"`
-	UserID     int       `json:"user_id" db:"user_id"`
+	ID         int            `json:"id" db:"id"`
+	Summary    string         `json:"summary" db:"summary"`
+	Start      time.Time      `json:"start" db:"start"`
+	Recurrence pq.StringArray `json:"recurrence" db:"recurrence"`
+	UserID     int            `json:"user_id" db:"user_id"`
 }
 
 type EventAction struct {
-	ID      int             `json:"id" db:"id"`
-	Name    EventActionName `json:"name" db:"name"`
-	Data    types.JSONText  `json:"data" db:"data"`
-	PlantID int             `json:"plant_id" db:"plant_id"`
-	EventID int             `json:"event_id" db:"event_id"`
+	ID      int            `json:"id" db:"id"`
+	Name    string         `json:"name" db:"name"`
+	Data    types.JSONText `json:"data" db:"data"`
+	PlantID int            `json:"plant_id" db:"plant_id"`
+	EventID int            `json:"event_id" db:"event_id"`
 }
 
-type EventActionName int
-
 const (
-	EventActionPlantWater EventActionName = iota
-	EventActionPlantCapturePhoto
+	EventActionPlantWater        = "PLANT_WATER"
+	EventActionPlantCapturePhoto = "PLANT_CAPTURE_PHOTO"
 )
