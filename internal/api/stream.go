qaisjp/growbot-api

commit e74840a8e2854ca572878f189ed30d5efd5c4113

Use integers for plant IDs

internal/api/stream.go
@@ -171,7 +171,7 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 
 		switch msg.Type {
 		case "PLANT_CAPTURE_PHOTO":
-			plantID := msg.Data["id"].(string)
+			plantID := int(msg.Data["id"].(float64))
 			plantImageB64 := msg.Data["image"].(string)
 
 			// todo: verify user owns plantID
@@ -180,7 +180,7 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 			filename := photoBucketKey(u)
 			photo := models.PlantPhoto{
 				Filename: u,
-				PlantID:  uuid.Must(uuid.Parse(plantID)),
+				PlantID:  plantID,
 			}
 
 			w, err := a.Bucket.NewWriter(ctx, filename, nil)
@@ -211,14 +211,9 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 
 		case "CREATE_LOG_ENTRY":
 			_, plantExists := msg.Data["plant_id"]
-			var plantID *uuid.UUID
+			var plantID int
 			if plantExists {
-				id, err := uuid.Parse(msg.Data["plant_id"].(string))
-				plantID = &id
-				if err != nil {
-					a.Log.WithField("data", msg.Data).WithError(err).Warnln("could not parse plant_id for CREATE_LOG_ENTRY")
-					continue
-				}
+				plantID = int(msg.Data["plant_id"].(float64))
 			}
 
 			var uid *int
@@ -239,7 +234,7 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 				Message:  msg.Data["message"].(string),
 				Severity: int(msg.Data["severity"].(float64)),
 				RobotID:  &rid,
-				PlantID:  plantID,
+				PlantID:  &plantID,
 			}
 
 			result, err := a.DB.NamedQuery("insert into log(user_id, type, message, severity, robot_id, plant_id) values (:user_id, :type, :message, :severity, :robot_id, :plant_id) returning id, created_at", entry)
@@ -267,11 +262,7 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 				continue
 			}
 
-			plantID, err := uuid.Parse(msg.Data["plant_id"].(string))
-			if err != nil {
-				a.Log.WithField("data", msg.Data).WithError(err).Warnln("could not parse plant_id for UPDATE_SOIL_MOISTURE")
-				continue
-			}
+			plantID := int(msg.Data["plant_id"].(float64))
 
 			plant := models.Plant{}
 			err = a.DB.Get(&plant, "select user_id from plants where id=$1", plantID)

internal/api/api.go
@@ -192,7 +192,7 @@ func NewAPI(
 		plants.GET("", a.PlantListGet)
 		plants.POST("", a.PlantCreatePost) // create a plant, only {name: ""}
 
-		plant := plants.Group("/:uuid", a.PlantCheck)
+		plant := plants.Group("/:id", a.PlantCheck)
 		{
 			plant.GET("", a.PlantGet)
 			plant.DELETE("", a.PlantDelete)

internal/api/log.go
@@ -23,7 +23,7 @@ type LogEntry struct {
 	Message   string     `json:"message" db:"message"`
 	Severity  int        `json:"severity" db:"severity"`
 	RobotID   *uuid.UUID `json:"robot_id,omitempty" db:"robot_id"`
-	PlantID   *uuid.UUID `json:"plant_id,omitempty" db:"plant_id"`
+	PlantID   *int       `json:"plant_id,omitempty" db:"plant_id"`
 	CreatedAt time.Time  `json:"created_at" db:"created_at"`
 }
 
@@ -33,7 +33,7 @@ func (a *API) LogListGet(c *gin.Context) {
 
 	input := struct {
 		RobotID  *string `form:"robot_id"`
-		PlantID  *string `form:"plant_id"`
+		PlantID  *int    `form:"plant_id"`
 		Severity *int    `form:"severity"`
 		Limit    int     `form:"limit,default=10"`
 		Offset   int     `form:"offset,default=0"`

internal/api/plants.go
@@ -2,8 +2,8 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
-	"github.com/google/uuid"
 	"github.com/teamxiv/growbot-api/internal/models"
 
 	"github.com/gin-gonic/gin"
@@ -12,8 +12,8 @@ import (
 // PlantCheck is a middleware to check whether the passed plant uuid exists,
 // and (if logged in) confirms whether the currently logged in user owns that plant
 func (a *API) PlantCheck(c *gin.Context) {
-	id := c.Param("uuid")
-	rid, err := uuid.Parse(id)
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		BadRequest(c, err.Error())
 		c.Abort()
@@ -21,7 +21,7 @@ func (a *API) PlantCheck(c *gin.Context) {
 	}
 
 	plant := models.Plant{}
-	err = a.DB.Get(&plant, "select * from plants where id = $1", rid)
+	err = a.DB.Get(&plant, "select * from plants where id = $1", id)
 	if err != nil {
 		BadRequest(c, "Plant does not exist ("+err.Error()+")")
 		c.Abort()
@@ -82,8 +82,7 @@ func (a *API) PlantDelete(c *gin.Context) {
 // PlantCreatePost gets the plant object
 func (a *API) PlantCreatePost(c *gin.Context) {
 	input := struct {
-		ID   uuid.UUID `json:"id,omitempty"`
-		Name string    `json:"name"`
+		Name string `json:"name"`
 	}{}
 
 	err := c.BindJSON(&input)
@@ -92,37 +91,30 @@ func (a *API) PlantCreatePost(c *gin.Context) {
 		return
 	}
 
-	if input.ID == uuid.Nil {
-		input.ID = uuid.New()
-	} else {
-		// Check the given UUID doesn't already exist
-		var count int
-		err := a.DB.Get(&count, "select count(id) from plants where id = $1", input.ID)
-		if err != nil {
-			a.error(c, http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		if count > 0 {
-			a.error(c, http.StatusBadRequest, "This plant is already in the database.")
-			return
-		}
-	}
-
 	row := models.Plant{
-		ID:     input.ID,
 		Name:   input.Name,
 		UserID: c.GetInt("user_id"),
 	}
 
-	_, err = a.DB.NamedQuery("insert into plants(id, name, user_id) values (:id, :name, :user_id)", row)
+	result, err := a.DB.NamedQuery("insert into plants(name, user_id) values (:name, :user_id) returning id", row)
 	if err != nil {
 		a.error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 
+	if !result.Next() {
+		a.error(c, http.StatusInternalServerError, "Expected result.Next() to return true")
+		return
+	}
+
+	var id int
+	if err := result.StructScan(&id); err != nil {
+		a.error(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"id": input.ID,
+		"id": id,
 	})
 }
 

internal/models/plants.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Plant struct {
-	ID           uuid.UUID `json:"id" db:"id"`
-	Name         string    `json:"name" db:"name"`
-	UserID       int       `json:"user_id" db:"user_id"`
-	SoilMoisture *int      `json:"soil_moisture" db:"soil_moisture"`
+	ID           int    `json:"id" db:"id"`
+	Name         string `json:"name" db:"name"`
+	UserID       int    `json:"user_id" db:"user_id"`
+	SoilMoisture *int   `json:"soil_moisture" db:"soil_moisture"`
 }
 
 type PlantPhoto struct {
 	ID        int       `json:"id" db:"id"`
 	Filename  uuid.UUID `json:"filename" db:"filename"`
-	PlantID   uuid.UUID `json:"plant_id" db:"plant_id"`
+	PlantID   int       `json:"plant_id" db:"plant_id"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }

schema.sql
@@ -54,7 +54,7 @@ CREATE TABLE public.event_actions (
     id integer NOT NULL,
     name public.event_action_name NOT NULL,
     data jsonb NOT NULL,
-    plant_id uuid,
+    plant_id integer,
     event_id integer NOT NULL,
     robot_id uuid NOT NULL
 );
@@ -131,7 +131,7 @@ CREATE TABLE public.log (
     message text NOT NULL,
     severity integer NOT NULL,
     robot_id uuid,
-    plant_id uuid,
+    plant_id integer,
     created_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
@@ -168,7 +168,7 @@ CREATE TABLE public.plant_photos (
     id integer NOT NULL,
     created_at timestamp without time zone DEFAULT now() NOT NULL,
     filename uuid NOT NULL,
-    plant_id uuid NOT NULL
+    plant_id integer NOT NULL
 );
 
 
@@ -201,7 +201,7 @@ ALTER SEQUENCE public.plant_photos_id_seq OWNED BY public.plant_photos.id;
 --
 
 CREATE TABLE public.plants (
-    id uuid NOT NULL,
+    id integer NOT NULL,
     user_id integer NOT NULL,
     name text NOT NULL,
     soil_moisture integer
@@ -209,6 +209,28 @@ CREATE TABLE public.plants (
 
 
 ALTER TABLE public.plants OWNER TO growbot;
+
+--
+-- Name: plants_id_seq; Type: SEQUENCE; Schema: public; Owner: growbot
+--
+
+CREATE SEQUENCE public.plants_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.plants_id_seq OWNER TO growbot;
+
+--
+-- Name: plants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: growbot
+--
+
+ALTER SEQUENCE public.plants_id_seq OWNED BY public.plants.id;
+
 
 --
 -- Name: robot_state; Type: TABLE; Schema: public; Owner: growbot
@@ -316,6 +338,13 @@ ALTER TABLE ONLY public.log ALTER COLUMN id SET DEFAULT nextval('public.log_id_s
 --
 
 ALTER TABLE ONLY public.plant_photos ALTER COLUMN id SET DEFAULT nextval('public.plant_photos_id_seq'::regclass);
+
+
+--
+-- Name: plants id; Type: DEFAULT; Schema: public; Owner: growbot
+--
+
+ALTER TABLE ONLY public.plants ALTER COLUMN id SET DEFAULT nextval('public.plants_id_seq'::regclass);
 
 
 --

commit c96205487a38a9f73d523cd039a0df939d6e38ca

stream: fix log entry creation
broken by e74840a8e2854ca572878f189ed30d5efd5c4113

internal/api/stream.go
@@ -211,9 +211,10 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 
 		case "CREATE_LOG_ENTRY":
 			_, plantExists := msg.Data["plant_id"]
-			var plantID int
+			var plantID *int
 			if plantExists {
-				plantID = int(msg.Data["plant_id"].(float64))
+				id := int(msg.Data["plant_id"].(float64))
+				plantID = &id
 			}
 
 			var uid *int
@@ -234,7 +235,7 @@ func (a *API) StreamRobot(ctx *gin.Context) {
 				Message:  msg.Data["message"].(string),
 				Severity: int(msg.Data["severity"].(float64)),
 				RobotID:  &rid,
-				PlantID:  &plantID,
+				PlantID:  plantID,
 			}
 
 			result, err := a.DB.NamedQuery("insert into log(user_id, type, message, severity, robot_id, plant_id) values (:user_id, :type, :message, :severity, :robot_id, :plant_id) returning id, created_at", entry)